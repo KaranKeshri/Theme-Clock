2dce987519943f27fef60e35ac21750a0fcd5e28
Karan Keshri <karankeshri071@gmail.com>
2023-04-30 15:11:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const hourEl = document.querySelector('.hour')
+
+
 const minuteEl = document.querySelector('.minute')
+
+
+
 const secondEl = document.querySelector('.second')
 const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('.date')
@@ -8,6 +13,8 @@ const toggle = document.querySelector('.toggle')
 
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 
@@ -16,13 +23,19 @@ toggle.addEventListener('click', (e) => {
     
     
     const html = document.querySelector('html')
+    
     if (html.classList.contains('dark')) {
+        
         html.classList.remove('dark')
+        
         e.target.innerHTML = 'Dark mode'
+        
     } else {
         
         
         html.classList.add('dark')
+        
+        
         e.target.innerHTML = 'Light mode'
     }
 })
@@ -33,20 +46,31 @@ function setTime() {
     
     
     const time = new Date();
+    
     const month = time.getMonth()
+    
     const day = time.getDay()
+    
     const date = time.getDate()
+    
     const hours = time.getHours()
+    
     const hoursForClock = hours >= 13 ? hours % 12 : hours;
     const minutes = time.getMinutes()
+    
     const seconds = time.getSeconds()
+    
     const ampm = hours >= 12 ? 'PM' : 'AM'
     
     
     
 
     hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`
+    
+    
     minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
+    
+    
     
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
     
